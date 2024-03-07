39b76bfd18f74cdd5ccdb19ea54f73f91ac62d59
AniketAG11 <guptaarti9903331832@gmail.com>
2024-03-07 23:31:26 +0530

    java scrpit

---
+++ JAVA_SCRIPT/01-variable.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "ANIKETGUPTA@google.com"
+var accountPassword = "123456"
+accountCity = "jaipur"
+
+//accountId = 2//not allowed 
+ console.log(accountId); 
+ /*
+ prefer not to use var 
+ because of issue in block scope and functional scope 
+ */            
