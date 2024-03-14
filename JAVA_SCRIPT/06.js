712f40109bce964e78bdeed95609f6d6bd55c179
AniketAG11 <guptaarti9903331832@gmail.com>
2024-03-14 23:34:15 +0530

    js

--- JAVA_SCRIPT/06.js
+++ JAVA_SCRIPT/06.js
@@ -1,6 +1,15 @@
 const score = 400
-console.log(score);
+//console.log(score);
 const balance = new Number(100)
-console.log(balance);
-console.log(balance.toString().length); 
-console.log(balance.toFixed(1));
+//console.log(balance);
+//console.log(balance.toString().length); 
+//console.log(balance.toFixed(1));
+const otherNumber = 123.5680
+//console.log(otherNumber.toPrecision(4));
+const hundred =100000
+//console.log(hundred.toLocaleString('en-IN'));
+//+++++++++++++++++++++++++----MATHS----+++++++++++++
+console.log(Math); 
+console.log(Math.abs(-4));
+console.log(Math.round(4.4));
+
